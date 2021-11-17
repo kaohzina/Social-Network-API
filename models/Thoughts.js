8c4654370c7a458f76564efb212304a2c8bf86ff
kaohzina <zinanshin.kaoher@gmail.com>
2021-11-17 12:20:43 -0600

    two models

---
+++ models/Thoughts.js
@@ -0,0 +1,26 @@
+const { Schema, model, Types } = require('mongoose');
+
+const thoughtsSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    length: 1<280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: createdAtVal => dateFormat(createdAtVal)
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: []
+});
+
+thoughtsSchema.virtual('reactionCount').get(function(){
+  return this.reactions.reduce((total, reaction) => total + reaction.replies.length + 1, 0);
+});
+
+const Thoughts = model('Thoughts', thoughtsSchema);
+module.exports = Thoughts;
---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model, Types } = require('mongoose');
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    unique: true,
+    require: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    //validator place holder
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+});
+
+userSchema.virtual('friendCount').get(
+  function() {
+    return this.friends.reduce((total, friends) => total + friends.length + 1, 0);
+  }
+)
+
+const Username = model('Username', userSchema);
+
+module.exports = Username;
